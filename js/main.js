b44946319c03ecb0b2d732a2b71a855594c38d89
Nicolas Coureau <ncoureau@gmail.com>
2020-08-04 19:34:39 +0200

    Mise en place de la class Map

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+    const newMap = new Map(10, 10, 0, 0);
+    newMap.genererMap();
     
-  
   });
--- js/map.js
+++ js/map.js
@@ -1,27 +1,31 @@
-
-const nbLignes = 10;
-const nbColonnes = 10;
-let x = 0;
-let y = 0;
-
-for (let i = 0; i < nbLignes; i++) {
-    const trElt = document.createElement('tr');
-    trElt.id = 'line-' + i;
-    document.getElementById('gameMap').appendChild(trElt);
-
-
-    for (let j = 0; j < nbColonnes; j++) {
-        const tdElt = document.createElement('td');
-        tdElt.id = x + '-' + y;
-        tdElt.innerHTML = `${i}-${j}`;
-        document.getElementById(`line-${y}`).appendChild(tdElt);
-        x++; // aller à droite
-
-        // passer à la ligne
-        if(document.getElementById(`line-${y}`).children.length == nbColonnes) {
-            y++;
-            x = 0;
-        }
+class Map {
+    constructor(nbLignes, nbColonnes, x, y) {
+        this.nbLignes = nbLignes;
+        this.nbColonnes = nbColonnes;
+        this.x = x;
+        this.y = y;
     }
-    
+    // Méthode de génération d'une map vierge
+    genererMap() {
+        // Création des lignes
+        for (let i = 0; i < this.nbLignes; i++) {
+            const trElt = document.createElement('tr');
+            trElt.id = 'line-' + i;
+            document.getElementById('gameMap').appendChild(trElt);        
+            
+            // Création des cellules
+            for (let j = 0; j < this.nbColonnes; j++) {
+                const tdElt = document.createElement('td');
+                tdElt.id = this.x + '-' + this.y;
+                document.getElementById(`line-${this.y}`).appendChild(tdElt);
+                this.x++; // Remplir vers la droite
+        
+                // Passer à la ligne
+                if(document.getElementById(`line-${this.y}`).children.length == this.nbColonnes) {
+                    this.y++; // On descend d'une ligne
+                    this.x = 0; // On recommence à gauche
+                }
+            }  
+        }    
+    }
 }

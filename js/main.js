18125c36ddafc74ed6e5e644bbc945d19d599bd0
Nicolas Coureau <ncoureau@gmail.com>
2020-09-22 19:00:04 +0200

    Modifications des chemins des images du tableau

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@ $(document).ready(function () {
     const partie = new Game();
 
     // Création des armes avec la classe Weapon (fichier weapons.js)
-    const epee = new Weapon("Epée",'<img src="../img/epee2-min.png" alt="epee" class="imgTableau">', 20);
-    const hache = new Weapon("Hache",'<img src="../img/hache2-min.png" alt="hache" class="imgTableau">', 20);
-    const arc = new Weapon("Arc",'<img src="../img/arc2-min.png" alt="arc" class="imgTableau">', 30);
-    const arbalete = new Weapon("Arbalète",'<img src="../img/arbalete2-min.png" alt="arbalete" class="imgTableau">', 30);
+    const epee = new Weapon("Epée",'<img src="img/epee2-min.png" alt="epee" class="imgTableau">', 20);
+    const hache = new Weapon("Hache",'<img src="img/hache2-min.png" alt="hache" class="imgTableau">', 20);
+    const arc = new Weapon("Arc",'<img src="img/arc2-min.png" alt="arc" class="imgTableau">', 30);
+    const arbalete = new Weapon("Arbalète",'<img src="img/arbalete2-min.png" alt="arbalete" class="imgTableau">', 30);
 
     // On crée un tableau avec les armes qui seront positionnées sur la map.
     const tableauArmes = [epee, hache, arc, arbalete];
 
     // Création des joueurs avec la class Player.
-    const joueurA = new Player("Le Lutin",'<img src="../img/lutin2-min.png" alt="lutin" class="imgTableau">', 100);
-    const joueurB = new Player("La Lutine", '<img src="../img/lutine2-min.png" alt="lutine" class="imgTableau">',100);
+    const joueurA = new Player("Le Lutin",'<img src="img/lutin2-min.png" alt="lutin" class="imgTableau">', 100);
+    const joueurB = new Player("La Lutine", '<img src="img/lutine2-min.png" alt="lutine" class="imgTableau">',100);
 
     // On crée un tableau avec les joueurs qui seront positionnés sur la map.
     const tableauJoueurs = [joueurA, joueurB];
